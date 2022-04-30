fcf2e055685394c33e18683164290af86ab6a848
Stavitskii <pochta100@mail.ru>
2022-04-30 20:38:42 +0300

    comments added

--- 11HW/Candidates/main.py
+++ 11HW/Candidates/main.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def list_candidates():
+    """Главная страница,
+    показывает всех кандидатов"""
     candidates = utils.load_candidates_from_json("candidates.json")
     return render_template("list.html", candidates=candidates)
 
 @app.route("/candidate/<int:candidate_id>")
 def page_candidate(candidate_id):
+    """Страница одного кандидата"""
     candidate = utils.get_candidate(candidate_id)
     if candidate:
         return render_template("card.html", candidate=candidate)
@@ -19,11 +22,13 @@ def page_candidate(candidate_id):
 
 @app.route("/search/<string:candidate_name>")
 def get_candidates_by_name(candidate_name):
+    """Поиск кандидатов по навыку"""
     candidates = utils.get_candidates_by_name(candidate_name)
     return render_template("search.html", candidates=candidates, candidates_count=len(candidates))
 
 @app.route("/skill/<string:skill_name>")
 def get_candidates_by_skill(skill_name):
+    """Поиск кандидатов по имени"""
     candidates = utils.get_candidates_by_skill(skill_name)
     return render_template("skill.html", candidates=candidates, candidates_count=len(candidates))
 
