50ac5088ae4dab47d1657ac7712189dfc040b087
Stavitskii <pochta100@mail.ru>
2022-04-30 20:32:10 +0300

    all works )

--- 11HW/Candidates/main.py
+++ 11HW/Candidates/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 import utils
 
 app = Flask(__name__)
@@ -8,6 +8,25 @@ app = Flask(__name__)
 def list_candidates():
     candidates = utils.load_candidates_from_json("candidates.json")
     return render_template("list.html", candidates=candidates)
+
+@app.route("/candidate/<int:candidate_id>")
+def page_candidate(candidate_id):
+    candidate = utils.get_candidate(candidate_id)
+    if candidate:
+        return render_template("card.html", candidate=candidate)
+    else:
+        abort(404)
+
+@app.route("/search/<string:candidate_name>")
+def get_candidates_by_name(candidate_name):
+    candidates = utils.get_candidates_by_name(candidate_name)
+    return render_template("search.html", candidates=candidates, candidates_count=len(candidates))
+
+@app.route("/skill/<string:skill_name>")
+def get_candidates_by_skill(skill_name):
+    candidates = utils.get_candidates_by_skill(skill_name)
+    return render_template("skill.html", candidates=candidates, candidates_count=len(candidates))
+
 
 
 app.run()
