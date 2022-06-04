d72ab5ed1e17181c4787750450c138774f5c2901
Stavitskii <pochta100@mail.ru>
2022-06-04 21:36:53 +0300

    12HW add post works, post_uploaded.html editing needed

--- lesson12/loader/loader.py
+++ lesson12/loader/loader.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, send_from_directory
-#from utils import get_posts
+from loader.utils import get_posts
 import logging
-ALLOWED_EXTENSIONS = {"jpg", "png", "jpeg"}
+ALLOWED_EXTENSIONS = {"jpg", "png", "jpeg", }
 
 loader_blueprint = Blueprint('loader_blueprint', __name__, template_folder='templates')
 
@@ -12,25 +12,28 @@ def upload_page():
     return render_template("post_form.html")
 
 
-#@loader_blueprint.route("/post", methods=["POST"])
-#def upload_done():
-#    try:
-#        picture = request.files.get("picture")
-#        content = request.form.get("content")
-#        filename = picture.filename
-#        extension = filename.split(".")[-1]
-#        if extension in ALLOWED_EXTENSIONS:
-#            picture.save(f"uploads/images/{filename}")
-#        else:
-        #    logging.info(f"Ошибка. Попытка загрузки файла формата '{extension}'")
-#            return f"<pre><h2> Упс! Файлы формата {extension} не поддерживаются. <h2></pre>"
-#        picture_url = f"./../uploads/images/{filename}"
-#        get_posts('posts.json', picture_url, content)
-#    except FileNotFoundError:
-        #logging.error("Ошибка при загрузке файла")
-#        return "<pre><h2> Ошибка загрузке файла. Попробуйте ещё раз <h2></pre>"
-#    else:
-#        return render_template("post_uploaded.html", filename=filename, content=content, picture_url=picture_url)
+@loader_blueprint.route("/post", methods=["POST"])
+def upload_done():
+    try:
+        picture = request.files.get("picture")
+        content = request.form.get("content")
+        if not picture or not content:
+            logging.info("Not enough data")
+            return "Not enough data"
+        filename = picture.filename
+        extension = filename.split(".")[-1]
+        if extension.lower() in ALLOWED_EXTENSIONS:
+            picture.save(f"uploads/images/{filename}")
+        else:
+            logging.info(f"Ошибка. Попытка загрузки файла формата '{extension}'")
+            return f"<pre><h2> Упс! Файлы формата {extension} не поддерживаются. <h2></pre>"
+        picture_url = f"./../uploads/images/{filename}"
+        get_posts('posts.json', picture_url, content)
+    except FileNotFoundError:
+        logging.error("Ошибка при загрузке файла")
+        return "<pre><h2> Ошибка загрузке файла. Попробуйте ещё раз <h2></pre>"
+    else:
+        return render_template("post_uploaded.html", filename=filename, content=content, picture_url=picture_url)
 
 @loader_blueprint.route("/uploads/<path:path>")
 def static_dir(path):
